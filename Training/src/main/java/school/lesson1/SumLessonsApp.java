c1b7ba01b7278c360191e4effde4297c8a9cceee
Kettika <liholat.ekaterina@gmail.com>
2024-07-11 22:40:38 +0300

    Lesson 1

--- Training/src/main/java/school/lesson1/SumLessonsApp.java
+++ Training/src/main/java/school/lesson1/SumLessonsApp.java
@@ -14,29 +14,29 @@ public class SumLessonsApp {
 
     static void checkSumSign () {
         int a = 7;
-        int b = -15;
+        int b = -9;
         if ((a + b) >= 0)
             System.out.println("Сумма положительная");
-        if ((a + b) < 0)
+        else
             System.out.println("Сумма отрицательная");
     }
 
      static void printColor() {
-        int c = 456;
-        if (c <=0)
+        int c = -1489;
+        if (c <= 0)
             System.out.println("Красный");
-        if (c>0 & c<=100)
+        if (c > 0 & c <= 100)
              System.out.println("Желтый");
-        if(c >100)
+        if(c > 100)
              System.out.println("Зеленый");
      }
 
      static void compareNumbers() {
         int a = -573;
         int b = -87;
-        if(a>=b)
-            System.out.println("a>=b");
+        if(a >= b)
+            System.out.println("a >= b");
         else
-            System.out.println("a<b");
+            System.out.println("a < b");
      }
 }
