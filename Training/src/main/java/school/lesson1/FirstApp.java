a590acd347f181b5d88f680bf3e9feddd94ef095
Kettika <liholat.ekaterina@gmail.com>
2024-07-11 09:09:51 +0300

    Initial commit

---
+++ Training/src/main/java/school/lesson1/FirstApp.java
@@ -0,0 +1,12 @@
+package school.lesson1;
+
+public class FirstApp {
+    public static void main(String[] args){
+        printThreeWords();
+
+    }
+    static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple\n");
+    }
+}
+
